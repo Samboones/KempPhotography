17046b8a2b5783f9860aa284369def5b4fe7e9cf
Samantha Eisenzimmer <140875221+Samboones@users.noreply.github.com>
2024-06-14 13:11:21 -0700

    Established all connections.

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1">Jodie Kemp Photography</span>
+        </div>
+      </nav>
+
+
+
+    </>
+  )
+}
+
+export default App
